000c45feea906e0786f09eb7762fcf1ca46f1998
Byeon-MJ <byunmj24@gmail.com>
2024-07-23 19:39:21 +0900

    질문 가져오기 서비스 추가

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,16 +1,25 @@
 package com.mysite.sbb.question;
 
+import com.mysite.sbb.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor    // 생성자 주입으로 Dependency Injection
 @Service
 public class QuestionService {
     private final QuestionRepository questionRepository;
-
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
