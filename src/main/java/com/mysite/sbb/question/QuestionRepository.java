b84fff45ed4a4f5d2dcd2902276e244709be7a09
Byeon_MJ <byunmj24@gmail.com>
2025-03-14 17:02:00 +0900

    3-13 검색 기능 추가하기  - JPA Specification 사용  - 질문 리포지터리 수정  - 질문 컨트롤러 수정  - 검색 화면 구현

--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -2,13 +2,15 @@ package com.mysite.sbb.question;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
-    Question findBySubject (String subject);    // findBySubject 메소드 추가(기본으로 제공되지 않음)
-    Question findBySubjectAndContent (String subject, String content);
-    List<Question> findBySubjectLike (String subject);
-    Page<Question> findAll (Pageable pageable);
+    Question findBySubject(String subject);    // findBySubject 메소드 추가(기본으로 제공되지 않음)
+    Question findBySubjectAndContent(String subject, String content);
+    List<Question> findBySubjectLike(String subject);
+    Page<Question> findAll(Pageable pageable);
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 }
